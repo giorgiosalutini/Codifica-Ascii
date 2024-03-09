7575c307caeb9a7df608c7272703fa30ed35e70b
giorgio.salutini <giorgio.salutini@gmail.com>
2024-03-09 11:43:09 +0100

    prima modifica

--- Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
+++ Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf9659ec8653290dcc99dadc8d73da154ddb593")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
